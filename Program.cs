97b79d998386dfcb71d81bf728b933b3cef0f7ef
valdemarCJ <vcj@ruc.dk>
2025-12-15 16:12:01 +0100

    Tilføjet en cors mere!

--- Program.cs
+++ Program.cs
@@ -181,6 +181,7 @@ builder.Services.AddCors(options =>
             .WithOrigins(
                 "http://localhost:3000", "https://localhost:3000",  // React default ports
                 "http://localhost:3001", "https://localhost:3001",
+				"http://localhost:5173", "https://localhost:5173",
 				"http://localhost:5173", "https://localhost:5173"
             )
             .AllowAnyMethod()
